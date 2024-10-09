85b1a3a99f8565961ff66337015a090f9f3771bb
arthur <arthur.desmonts@gmail.com>
2024-10-09 17:34:14 +0200

    fix CORS issue for client reception

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import getRoutes from "./routes/get.routes.js";
 const port = 5500;
 
 const app = express();
 
+app.use(cors());
+
 //route for get request
 app.use('/get', getRoutes);
 
-app.listen(port, () => console.log("server ON -> port : " + port));
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
