39f8cae51cd87eeabb28cc1c420d2a9e991a4aa2
arthur <arthur.desmonts@gmail.com>
2024-10-16 01:40:37 +0200

    add mongoDB connection for vercel prod

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
     try {
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('Connected to the database');
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
     } catch (error) {
-        console.error(error);
+        console.error('MongoDB connection error:', error);
         process.exit(1);
     }
-}
+};
 
 export default connectDB;
