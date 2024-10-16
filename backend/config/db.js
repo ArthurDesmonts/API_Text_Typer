3bcaa84ce77cc504270faac5d9e9e740a71be371
arthur <arthur.desmonts@gmail.com>
2024-10-16 02:05:19 +0200

    change db.js for more logs to trouble shoot

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,13 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
+        const uri = process.env.MONGO_URI;
+        if (!uri) {
+            throw new Error('MONGO_URI is not defined');
+        }
+        console.log(`Connecting to MongoDB with URI: ${uri}`);
         mongoose.set('strictQuery', false);
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(uri);
         console.log('Connected to the database');
     } catch (error) {
         console.error(error);
