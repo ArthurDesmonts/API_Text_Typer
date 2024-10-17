9706a863ab5950fdb783b6a305a9089b58764718
arthur <arthur.desmonts@gmail.com>
2024-10-18 01:11:42 +0200

    add the creation date to the user models

--- backend/models/user.model.cjs
+++ backend/models/user.model.cjs
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     password: { type: String, required: true },
+    creationDate: { type: Date, required: false, default: Date.now },
     classement: { type: Number, required: false },
     recordWPM: { type: Number, required: false },
     moyenneWPM: { type: Number, required: false },
