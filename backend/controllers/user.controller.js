afc1eb4a4de0cf063987a57189ecf1a8377c2365
arthur <arthur.desmonts@gmail.com>
2024-10-22 15:26:56 +0200

    BUG Fix : Create a user with an indentical userName

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,25 +1,32 @@
 import User from "../models/user.model.cjs";
 import {hash, compare} from "bcrypt";
-import GameResults from "../models/gameResults.model.cjs";
 
-export const signup = (req, res, next) => {
-    hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                name: req.body.name,
-                password: hash,
-                classement: 0,
-                recordWPM: 0,
-                moyenneWPM: 0
-            });
-            user.save()
-                .then(() => res.status(201).json({ userId: user._id }))
-                .catch(error => res.status(400).json({ error }));
+export const signup = (req, res) => {
+    User.findOne({ name: req.body.name })
+        .then(existingUser => {
+            if (existingUser) {
+                return res.status(400).json({ message: 'Username already exists' });
+            }
+
+            hash(req.body.password, 10)
+                .then(hash => {
+                    const user = new User({
+                        name: req.body.name,
+                        password: hash,
+                        classement: 0,
+                        recordWPM: 0,
+                        moyenneWPM: 0
+                    });
+                    user.save()
+                        .then(() => res.status(201).json({ userId: user._id }))
+                        .catch(error => res.status(400).json({ error }));
+                })
+                .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
 };
 
-export const login = (req, res, next) => {
+export const login = (req, res) => {
     User.findOne({ name: req.body.name })
         .then(user => {
             if (!user) {
@@ -53,25 +60,6 @@ export const getUserInfo = (req, res, next) => {
                 recordWPM: user.recordWPM,
                 moyenneWPM: user.moyenneWPM
             });
-        })
-        .catch(error => res.status(500).json({ error }));
-};
-
-export const refreshUserData = (req, res, next) => {
-    const userId = req.query.userId;
-    const user = {
-        recordWPM: 0,
-        moyenneWPM: 0
-    };
-
-    GameResults.find({userId: userId})
-        .then(results => {
-            if (results.length === 0) {
-                return res.status(200).json(user);
-            }
-            user.recordWPM = results.reduce((max, result) => result.wpm > max ? result.wpm : max, 0);
-            user.moyenneWPM = results.reduce((sum, result) => sum + result.wpm, 0) / results.length;
-            res.status(200).json(user);
         })
         .catch(error => res.status(500).json({ error }));
 };
